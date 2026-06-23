f8e9092b3d7b266478f069d9071a12f4b65860eb
JulianaOAguilar <julianaoaguilar16@gmail.com>
2026-06-23 13:03:19 -0300

    conexao com o supabase

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,13 +1,28 @@
+import { useEffect } from "react"
 import { View, Text, Button } from "react-native"
 import type { NativeStackScreenProps } from "@react-navigation/native-stack"
 import type { RootStackParamList } from "../navigation/AppNavigator"
+import { supabase } from "../services/supabase"
 
 type Props = NativeStackScreenProps<RootStackParamList, "Home">
 
 export default function HomeScreen({ navigation }: Props) {
+
+  useEffect(() => {
+    testarSupabase()
+  }, [])
+
+  async function testarSupabase() {
+    const { data, error } = await supabase.auth.getSession()
+
+    console.log("DATA:", data)
+    console.log("ERROR:", error)
+  }
+
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text>🚛 Home - Paradas</Text>
+
       <Button
         title="Ir para Detalhes"
         onPress={() => navigation.navigate("Details")}
---
+++ src/services/supabase.ts
@@ -0,0 +1,9 @@
+// supabase.ts
+
+import { createClient } from '@supabase/supabase-js'
+
+export const supabase = createClient(
+  process.env.EXPO_PUBLIC_SUPABASE_URL!,
+  process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY!
+)
+
