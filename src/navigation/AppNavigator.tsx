ee5e72367312fcbedad05b55f53324f95b731c4e
Julianaoaguilar <julianaoaguilar16@gmail.com>
2026-06-24 09:39:08 -0300

    Login

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -1,17 +1,19 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "../screens/HomeScreen";
 import StopDetailsScreen from "../screens/StopDetailsScreen";
+import Authentication from "../screens/Authentication";
+import Register from "../screens/Register";
 import { RootStackParamList } from "./types";
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function AppNavigator() {
   return (
-    <Stack.Navigator>
+    <Stack.Navigator initialRouteName="Login">
+      <Stack.Screen name="Login" component={Authentication} />
+      <Stack.Screen name="Cadastro" component={Register} />
       <Stack.Screen name="Home" component={HomeScreen} />
       <Stack.Screen name="Details" component={StopDetailsScreen} />
-      <Stack.Screen name="Home" component={HomeScreen} />
-       <Stack.Screen name="Details" component={StopDetailsScreen} />
     </Stack.Navigator>
   );
 }
