0dafa605836de25917f019a117a2aa020f94add3
Julianaoaguilar <julianaoaguilar16@gmail.com>
2026-06-24 08:59:22 -0300

    baixando uma versao mais antiga do expo

--- src/screens/StopDetailsScreen.tsx
+++ src/screens/StopDetailsScreen.tsx
@@ -1,10 +1,15 @@
 import { View, Text, Button } from "react-native"
 import type { NativeStackScreenProps } from "@react-navigation/native-stack"
-import type { RootStackParamList } from "../navigation/AppNavigator"
+import { RootStackParamList } from "../navigation/types";
+
+
+
 
 type Props = NativeStackScreenProps<RootStackParamList, "Details">
 
 export default function StopDetailsScreen({ navigation }: Props) {
+
+
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text>📍 Detalhes da Parada</Text>
